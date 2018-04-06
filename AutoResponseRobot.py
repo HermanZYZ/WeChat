ee6376fe020f06b37bc1c5983847e8651a8c79ab
HermanZYZ <820421356@qq.com>
2018-04-06 16:50:21 +0800

    Update AutoResponseRobot.py

--- AutoResponseRobot.py
+++ AutoResponseRobot.py
@@ -25,6 +25,14 @@ def get_response(msg):
         return r.get('text')
     except:
         return '爱你么么哒'
+    
+##将语音消息转成文本返回
+def voiceToText(msg):
+    audio = AudioSegment.from_mp3(msg['FileName'])
+    export = audio.export(format="amr", bitrate="12.20k")
+    transform = aipSpeech.asr(export.read(), 'amr', 8000, {'lan': 'zh', })
+    print(transform)
+    return transform
 
 '''自动回复'''
 @itchat.msg_register([TEXT, PICTURE, MAP, CARD, NOTE, SHARING, RECORDING, ATTACHMENT, VIDEO])
@@ -34,10 +42,11 @@ def getMSG(msg):
     print(msg['MsgType'])
     if msg['FromUserName'] != None:
         if msg['MsgType'] == 34: #语音消息
-            print(msg['Text'](msg['FileName']))
-            audio = AudioSegment.from_mp3(msg['FileName'])
-            export = audio.export(format="amr", bitrate="12.20k")
-            transform = aipSpeech.asr(export.read(), 'amr', 8000, {'lan': 'zh', })
+#             print(msg['Text'](msg['FileName']))
+#             audio = AudioSegment.from_mp3(msg['FileName'])
+#             export = audio.export(format="amr", bitrate="12.20k")
+#             transform = aipSpeech.asr(export.read(), 'amr', 8000, {'lan': 'zh', })
+            transform = voiceToText(msg)
             if transform['err_msg'] == 'success.':
                 print(transform['result'][0])
                 itchat.send(get_response(transform['result'][0]), toUserName=msg['FromUserName'])
@@ -45,13 +54,6 @@ def getMSG(msg):
         elif msg['MsgType'] == 1: #文本消息
             itchat.send(get_response(msg['Text']), toUserName=msg['FromUserName'])
 
-##将语音消息转成文本返回
-def voiceToText(msg):
-    audio = AudioSegment.from_mp3(msg['FileName'])
-    export = audio.export(format="amr", bitrate="12.20k")
-    transform = aipSpeech.asr(export.read(), 'amr', 8000, {'lan': 'zh', })
-    print(transform)
-    return transform
 
 # print(get_response('你好'))
 if __name__ == '__main__':
